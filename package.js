6e8f080ad03eeb91c976af1e516d09b4753356ab
Jan Küster <jkuester@uni-bremen.de>
2019-03-15 00:01:32 +0100

    publish 1.0.0

--- package.js
+++ package.js
@@ -4,9 +4,9 @@ Package.describe({
   name: 'jkuester:autoform-trix',
   version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: 'Providing basecamp/trix for AutoForm',
+  summary: 'Get the Trix wysiwyg-editor as configurable AutoForm extension.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jankapunkt/meteor-autoform-trix.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -17,8 +17,6 @@ Package.onUse(function (api) {
   api.use('ecmascript')
   api.use('aldeed:autoform@6.0.0')
   api.use('templating@1.3.2')
-  api.use('random')
-  api.use('tracker')
   api.use('reactive-dict')
   api.addFiles([
     'autoform-trix.html',
@@ -26,11 +24,4 @@ Package.onUse(function (api) {
     'autoform-trix.css',
     'autoform-config.js'
   ], 'client')
-})
-
-Package.onTest(function (api) {
-  api.use('ecmascript')
-  api.use('tinytest')
-  api.use('jkuester:autoform-trix')
-  api.mainModule('autoform-trix-tests.js')
 })
